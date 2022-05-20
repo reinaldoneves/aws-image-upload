aacd06d3e8a6ab77f526b6fe1367d9188a6df823
Reinaldo Neves <reinaldo_neves@hotmail.com>
2022-05-20 17:25:07 -0300

    confirmar

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,7 +1,11 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useCallback} from "react";
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
+import { useDropzone } from 'react-dropzone'
+
+// Continuar daqui:
+// Sending to S3: https://youtu.be/i-hoSg8iRG0?t=4712
 
 const UserProfiles = () => {
 
@@ -21,12 +25,38 @@ const UserProfiles = () => {
   return userProfiles.map((userProfile, index) => {
     return (
         <div key={index}>
+          {/* todo: profile image */}
+          <br />
+          <br />
           <h1>{userProfile.username}</h1>
           <p>{userProfile.userProfileId}</p>
+          <Dropzone />
+          <br />
         </div>
     );
   });
 };
+
+function Dropzone() {
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+    //Call the post methodo on our api to upload to S3
+    const file = acceptedFiles[0];
+    console.log(file);
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the image here ...</p> :
+          <p>Drag 'n' drop profile image here, or click to select files</p>
+      }
+    </div>
+  )
+}
 
 function App() {
   return (
